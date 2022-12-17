366aa38d48563daa9903d66ab168b5cf50d669b5
Sean Siddens <seansiddens@gmail.com>
2022-12-17 13:16:53 -0800

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const About = (() => {
+  return (
+    <div>
+      <Toolbar />
+      <Typography variant='h1'>
+        About me
+      </Typography>
+    </div>
+  );
+});
+
+export default About;
---
+++ src/Portfolio.js
@@ -0,0 +1,11 @@
+import Toolbar from '@mui/material/Toolbar';
+const Portfolio = (() => {
+  return (
+    <>
+      <Toolbar />
+      Portfolio!
+    </>
+  );
+});
+
+export default Portfolio;
